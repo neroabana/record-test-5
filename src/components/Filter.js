7a0014dd9839acde3f5dac998da52c9c1e275545
abana <neroabana@gmail.com>
2020-12-14 17:01:01 +0100

    persisted record set

---
+++ src/components/Filter.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { PropTypes } from "prop-types";
+import "../styles/Filter.css";
+
+const Filter = ({ sortName, sortAge }) => {
+  return (
+    <>
+      <div id="control-box">
+        <div id="control-name">
+          <input
+            type="checkbox"
+            onChange={sortName}
+            name="name_check"
+            data-testid="name"
+          />{" "}
+          <label htmlFor="name_check">Name</label>
+        </div>
+        <div id="control-age">
+          <input
+            type="checkbox"
+            onClick={sortAge}
+            name="age_check"
+            data-testid="age"
+          />{" "}
+          <label htmlFor="age_check">Age</label>
+        </div>
+      </div>
+    </>
+  );
+};
+
+Filter.propTypes = {
+  sortName: PropTypes.func,
+  sortAge: PropTypes.func,
+};
+
+export default Filter;
