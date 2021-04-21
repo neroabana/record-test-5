6079b990926a98b2218b7fef3153735af071fbeb
abana <neroabana@gmail.com>
2021-04-21 13:36:42 +0100

    minor refactor

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Filter from "./components/Filter";
 import RecordTable from "./components/RecordTable";
 import "./styles/App.css";
 
-let people = [
-  
+let people = [  
   {
     name: "Veronica Mize",
     dob: "11/29/2011",
