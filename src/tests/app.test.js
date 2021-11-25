30f483be861875a457ba7f4d3f08600fd3482a31
Ali Abana <neroabana@gmail.com>
2021-11-25 11:06:34 +0100

    tt

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,9 +1,6 @@
 import React from "react";
-
 import {render} from "@testing-library/react";
-
 import userEvent from "@testing-library/user-event";
-
 import Filter from "./Filter";
 
 import RecordTable from "./RecordTable";
